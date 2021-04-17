c60ad33881950cad75df78ca820f9f15e6cd14ee
RZZhang6 <zrz240132771@qq.com>
2021-04-17 21:14:05 +0800

    新增ABC类

---
+++ meinian_common/src/main/java/com/atguigu/util/ABC.java
@@ -0,0 +1,11 @@
+package com.atguigu.util;
+
+/**
+ * @author RZZhang
+ * @create 2021-04-17 20:34
+ */
+public class ABC {
+    public static void main(String[] args) {
+        System.out.println("11111111");
+    }
+}
